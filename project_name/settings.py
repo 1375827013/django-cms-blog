1fe979ff4a6fb8a46a3daefc10e2d22bd540b387
chenjiaze <1375827013@qq.com>
2026-02-21 02:21:02 +0800

    fix: 兼容没有 python-dotenv 的环境

--- project_name/settings.py
+++ project_name/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
